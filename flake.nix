6f0c7804a10b2ff69ae62359a3ee1d72c62b8a54
nullstring1 <kernd2005@gmail.com>
2025-04-27 18:14:27 +0100

    Add parsing for btsnoop write commands

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Android DevShell FHS Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; config = {
+          allowUnfree = true;
+        };};
+        rustEnv = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "aarch64-linux-android" ];
+          extensions = [ "rust-std" "rust-src" ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rustEnv
+            pkgs.android-tools
+            pkgs.cargo-ndk
+            pkgs.autoPatchelfHook
+            pkgs.zlib
+            pkgs.libxml2
+            pkgs.lldb
+            pkgs.wireshark
+          ];
+        };
+      });
+}
